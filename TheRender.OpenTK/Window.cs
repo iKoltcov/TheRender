cd082c50a4f90549189c02d6e6b39769a778055f
Ivan Koltcov <ikoltcov@yandex.ru>
2019-10-07 18:27:51 +0300

    issue-3 // Add static class with default scene

--- TheRender.OpenTK/Window.cs
+++ TheRender.OpenTK/Window.cs
@@ -4,6 +4,7 @@ using OpenTK.Graphics;
 using OpenTK.Graphics.ES20;
 using TheRender.Entities;
 using TheRender.OpenTK.Entities;
+using TheRender.OpenTK.Scenes;
 using TheRender.OpenTK.Services;
 using TheRender.Services;
 
@@ -32,72 +33,7 @@ namespace TheRender.OpenTK
         public Window() : base(1024, 768, GraphicsMode.Default, "TheRender")
         {
             rayTracingService = new RayTracingService(cellWidth, cellHeight, countTask);
-            rayTracingService.AddLight(new PointLightEntity()
-            {
-                Position = new System.Numerics.Vector3(0.0f, 50.0f, 50.0f),
-                Intensity = 0.8f,
-                Color = new ColorEntity(1.0f, 1.0f, 1.0f)
-            });
-            rayTracingService.AddLight(new PointLightEntity()
-            {
-                Position = new System.Numerics.Vector3(-20.0f, 50.0f, 100.0f),
-                Intensity = 0.8f,
-                Color = new ColorEntity(1.0f, 1.0f, 1.0f)
-            });
-
-            rayTracingService.AddEssence(new SphereEntity
-            {
-                Material = MaterialEntity.Default,
-                Position = new System.Numerics.Vector3(-20.0f, 0.0f, 120.0f),
-                Radius = 20.0f
-            });
-            rayTracingService.AddEssence(new SphereEntity
-            {
-                Material = MaterialEntity.Default,
-                Position = new System.Numerics.Vector3(0.0f, -5.0f, 100.0f),
-                Radius = 15.0f
-            });
-            rayTracingService.AddEssence(new SphereEntity
-            {
-                Material = MaterialEntity.Default,
-                Position = new System.Numerics.Vector3(10.0f, -10.0f, 80.0f),
-                Radius = 10.0f
-            });
-            rayTracingService.AddEssence(new SphereEntity
-            {
-                Material = MaterialEntity.Default,
-                Position = new System.Numerics.Vector3(15.0f, -15.0f, 70.0f),
-                Radius = 5.0f
-            });
-            rayTracingService.AddEssence(new SphereEntity
-            {
-                Material = MaterialEntity.Default,
-                Position = new System.Numerics.Vector3(17.0f, -17.5f, 65.0f),
-                Radius = 2.5f
-            });
-            
-            rayTracingService.AddEssence(new SphereEntity
-            {
-                Material = MaterialEntity.Default,
-                Position = new System.Numerics.Vector3(18.0f, -18.75f, 62.5f),
-                Radius = 1.25f
-            });
-            
-            rayTracingService.AddEssence(new QuadEntity()
-            {
-                Material = new MaterialEntity()
-                {
-                    Color = ColorEntity.White,
-                    DiffuseComponent = 0.7f,
-                    ReflectComponent = 0.3f,
-                },
-                Position = new System.Numerics.Vector3(0.0f, -20.0f, 100.0f),
-                Normal = new System.Numerics.Vector3(0.0f, 1.0f, 0.0f),
-                VertexA = new System.Numerics.Vector3(-1000.0f, 0.0f,  1000.0f),
-                VertexB = new System.Numerics.Vector3(1000.0f, 0.0f,  1000.0f),
-                VertexC = new System.Numerics.Vector3(1000.0f, 0.0f,  -1000.0f),
-                VertexD = new System.Numerics.Vector3(-1000.0f, 0.0f,  -1000.0f)
-            });
+            rayTracingService.AddDefaultScene();
             
             shaderService = new ShaderService();
 
